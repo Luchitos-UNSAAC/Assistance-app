31fb498d85bd8bdef2fa2826f3038304c3b3dbb5
Edward Melendez <edward.melendez@limber.io>
2026-01-04 18:46:48 -0500

    upd: name field in me request

--- app/(root)/api/v2/users/me/route.ts
+++ app/(root)/api/v2/users/me/route.ts
@@ -12,6 +12,7 @@ export async function GET(req: Request) {
       where: { id: payload.userId },
       select: {
         id: true,
+        name: true,
         email: true,
         role: true,
         createdAt: true,
